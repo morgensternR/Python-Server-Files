2180aa9eeff943740d31e02d5a503e79c239f378
Morgenstern <rdm8@NIST.GOV>
2023-02-14 13:04:39 -0700

    Update Realtime_Graph.py

--- Realtime_Graph.py
+++ Realtime_Graph.py
@@ -2,87 +2,93 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #Real time graphing and Subscriber
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import time
+import numpy as np
+from matplotlib.cm import get_cmap
 import matplotlib.dates as mdates
 import zmq
 import ujson
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+import pandas
 
-def demogrify(topicmsg):
-    """ Inverse of mogrify() """
-    topicmsg = topicmsg.decode()
-    json0 = topicmsg.find('{')
-    topic = topicmsg[0:json0].strip()
-    msg = ujson.loads(topicmsg[json0:])
-    return topic, msg 
+
+def receive_data(socket, topicfilter):
+    return ujson.loads(socket.recv().decode().split(topicfilter)[1].strip()) 
 
 # Make Subscriber to Publisher with 'graph' set topic...
 
+print('Starting SUB Socket')
 port = "5556"
 context = zmq.Context()
 socket = context.socket(zmq.SUB)   
+socket.RCVTIMEO = -1
 socket.connect('tcp://132.163.53.82:%s' %port)
-topicfilter = "graph"
+topicfilter = 'graph'
 socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
 
+print('Starting Plotting Window')
+app = QtGui.QApplication([])
+win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
+win.resize(1000,600)
+win.setWindowTitle('pyqtgraph example: Plotting')
+# Enable antialiasing for prettier plots
+pg.setConfigOptions(antialias=True)
+p6 = win.addPlot(title="Updating plot")
+p6.addLegend()
 
-# Create figure for plotting
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-topic, messagedata = demogrify(socket.recv())
+sensor_data = receive_data(socket, topicfilter)
+print('Importing current Log File')
+log_file = pandas.read_csv('L:/ryan_python/Python_Server_Logger/thermometry_log.csv')
 
 
+def init(log_file):
+    curves = {}
+    color_map = get_cmap('hsv', len(sensor_data) - 1)
+    idx = 0
+    for key in log_file:
+        if key != 'TIME':
+            curves[key] = p6.plot(log_file['TIME'], log_file[key], name = key, pen = (np.array(color_map(idx))*255).astype(int) )
+            idx += 1
+    p6.enableAutoRange('xy', True)
+    return curves
 
-sensors_list = {}
-for key in messagedata:
-    if key not in 'TIME':
-        sensors_list[key] = []   
-xs = []
-# This function is called periodically from FuncAnimation
+#Making curve dictionary with sensor keys and plot values
+print('Plotting current Log file')
+curves = init(log_file)
 
-def animate(i, xs, sensor_list, pts):
-    #loops in while till new data
-    topic, messagedata = demogrify(socket.recv())
-    
-    # Add x and y to lists
-    xs.append(mdates.epoch2num(messagedata['TIME']))
-    xs = xs[-pts:]
-    ax.clear()
-    
-    
-    for key in messagedata:
+def update(data):
+    global curves 
+    '''
+    try:
+        title, data = demogrify(socket.recv())
+    except:
+        data = None
+    '''    
+    if data is not None:
+        for key in curves:
+            curve = curves[key]
+            x, y = np.append(curve.getData()[0], data['TIME']), np.append(curve.getData()[1], data[key])
         
-        if key not in 'TIME':
+            curve.setData(x,y)
+    
+
+## Start Qt event loop unless running in interactive mode or using pyside.
+class get_data_thread(pg.QtCore.QThread): #Starts QThread for data polling
+    newData = pg.QtCore.Signal(dict) #Defines signal data type
+    def run(self):
+        while True:
+            sensor_data = receive_data(socket, topicfilter)
+            # do NOT plot data from here!
+            self.newData.emit(sensor_data) #Emits signal to new.connect(slot)
             
-            sensor_list[key].append(messagedata[key])
-            sensor_list[key] = sensor_list[key][-pts:]
-            line = ax.plot_date(xs, sensor_list[key], linestyle = '--', label = key)
-        else:
-            pass
-        
-    #print(sensor_list)
-    # Draw x and y lists
-    
-    #line = ax.plot_date(xs, ys)
+data_getter = get_data_thread()
+data_getter.newData.connect(update) #Emits signal to update slot
+data_getter.start()#Starts thread
 
-# Format plot
-    # Choose your xtick format string
-    date_fmt = '%d-%m-%y %H:%M:%S'
-    
-    # Use a DateFormatter to set the data to the correct format.
-    date_formatter = mdates.DateFormatter(date_fmt)
-    ax.xaxis.set_major_formatter(date_formatter)
-    
-    # Sets the tick labels diagonal so they fit easier.
-    fig.autofmt_xdate()
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_() #Starts own thread for GUI
 
-    plt.title('Diode Sensor Data')
-    plt.ylabel('Voltage (V)')
-    ax.grid()
-    ax.legend(bbox_to_anchor=(1.01, 1.0), loc='upper left')
-    return #line
 
-# Set up plot to call animate() function periodically
-ani = FuncAnimation(fig, animate, fargs=(xs, sensors_list, 1000), interval=10000, blit=False)
-
-plt.show()
