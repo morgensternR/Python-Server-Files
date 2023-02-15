eeda0ced8bddd9f281a22278500dd64fdc27a708
Morgenstern <rdm8@NIST.GOV>
2023-02-15 11:35:37 -0700

    Update Realtime_Graph.py

--- Realtime_Graph.py
+++ Realtime_Graph.py
@@ -9,15 +9,15 @@ import matplotlib.dates as mdates
 import zmq
 import ujson
 from pyqtgraph.Qt import QtGui, QtCore
+from PyQt5.QtWidgets import QApplication, QWIDGETSIZE_MAX
+from pyqtgraph.Point import Point
 import pyqtgraph as pg
 import pandas
+from scipy.interpolate import interp1d
 
-
+# Make Subscriber to Publisher with 'graph' set topic...       
 def receive_data(socket, topicfilter):
     return ujson.loads(socket.recv().decode().split(topicfilter)[1].strip()) 
-
-# Make Subscriber to Publisher with 'graph' set topic...
-
 print('Starting SUB Socket')
 port = "5556"
 context = zmq.Context()
@@ -27,31 +27,53 @@ socket.connect('tcp://132.163.53.82:%s' %port)
 topicfilter = 'graph'
 socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
 
+# Start QtGui application
 print('Starting Plotting Window')
 app = QtGui.QApplication([])
+
+# Window formatting
 win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
-win.resize(1000,600)
+win.resize(1000,1000)
 win.setWindowTitle('pyqtgraph example: Plotting')
+label = pg.LabelItem(justify='right')
+win.addItem(label)
+
 # Enable antialiasing for prettier plots
 pg.setConfigOptions(antialias=True)
-p6 = win.addPlot(title="Updating plot")
-p6.addLegend()
 
-sensor_data = receive_data(socket, topicfilter)
+# Plot labeling
+p0 = win.addPlot(title="Thermometry Realtime + Log")
+p0.showGrid(x = True, y = True)
+p0.setLabel('left', 'Temperature (K)')
+date_axis = pg.DateAxisItem(orientation='bottom')
+p0.setAxisItems(({'bottom': date_axis}) )
+p0.setLabel('bottom', 'Date and Time (test)')
+p0.addLegend()
+
+# Import log file and calibration files. Change directory Here
 print('Importing current Log File')
 log_file = pandas.read_csv('L:/ryan_python/Python_Server_Logger/thermometry_log.csv')
-
+print('Importing Calibration Files')
+dc2018 = np.loadtxt('L:/ryan_python/DC2018.csv',  delimiter =',', dtype=float)
+dt670 = np.loadtxt('L:/ryan_python/DT670.csv',  delimiter =',', dtype=float)
+dt670_func = interp1d(dt670[:,1], dt670[:,0])
+dc2018_func = interp1d(dc2018[:,1], dc2018[:,0])
 
 def init(log_file):
     curves = {}
-    color_map = get_cmap('hsv', len(sensor_data) - 1)
+    color_map = get_cmap('hsv', len(log_file.keys()) - 1)
     idx = 0
     for key in log_file:
         if key != 'TIME':
-            curves[key] = p6.plot(log_file['TIME'], log_file[key], name = key, pen = (np.array(color_map(idx))*255).astype(int) )
+            color = tuple((np.array(color_map(idx))*255).astype(int))
+            if key == '4K' or key == '40K':
+                curves[key] = p0.plot(log_file['TIME'], dt670_func(log_file[key]), name = key, pen = color)#, symbol = 'o', symbolSize = 1)
+            else:
+                curves[key] = p0.plot(log_file['TIME'], dc2018_func(log_file[key]), name = key, pen = color)#, symbol = 'o' , symbolSize = 1)
             idx += 1
-    p6.enableAutoRange('xy', True)
+    p0.enableAutoRange('xy', False)
     return curves
+
 
 #Making curve dictionary with sensor keys and plot values
 print('Plotting current Log file')
@@ -59,20 +81,16 @@ curves = init(log_file)
 
 def update(data):
     global curves 
-    '''
-    try:
-        title, data = demogrify(socket.recv())
-    except:
-        data = None
-    '''    
     if data is not None:
         for key in curves:
             curve = curves[key]
-            x, y = np.append(curve.getData()[0], data['TIME']), np.append(curve.getData()[1], data[key])
-        
+            if key == '4K' or key == '40K':
+                x, y = np.append(curve.getData()[0], data['TIME']), np.append(curve.getData()[1], dt670_func(data[key]))
+            else:
+                x, y = np.append(curve.getData()[0], data['TIME']), np.append(curve.getData()[1], dc2018_func(data[key]))
+            
             curve.setData(x,y)
     
-
 ## Start Qt event loop unless running in interactive mode or using pyside.
 class get_data_thread(pg.QtCore.QThread): #Starts QThread for data polling
     newData = pg.QtCore.Signal(dict) #Defines signal data type
@@ -85,6 +103,27 @@ class get_data_thread(pg.QtCore.QThread): #Starts QThread for data polling
 data_getter = get_data_thread()
 data_getter.newData.connect(update) #Emits signal to update slot
 data_getter.start()#Starts thread
+
+
+#cross hair
+vLine = pg.InfiniteLine(angle=90, movable=False)
+hLine = pg.InfiniteLine(angle=0, movable=False)
+p0.addItem(vLine, ignoreBounds=True)
+p0.addItem(hLine, ignoreBounds=True)
+
+
+vb = p0.vb
+
+def mouseMoved(evt):
+    global curves
+    pos = evt[0]  ## using signal proxy turns original arguments into a tuple
+    if p0.sceneBoundingRect().contains(pos):
+        mousePoint = vb.mapSceneToView(pos)
+        vLine.setPos(mousePoint.x())
+        hLine.setPos(mousePoint.y())
+proxy = pg.SignalProxy(p0.scene().sigMouseMoved, rateLimit=60, slot=mouseMoved)
+
+
 
 if __name__ == '__main__':
     import sys
